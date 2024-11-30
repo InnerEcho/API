30e4a0a7a1ad9470c07ca64973cc99302d768192
YonghoBae <dyddyd134@chungbuk.ac.kr>
2024-11-30 22:10:23 +0900

    feat: plant state check add

--- src/controllers/plant.ts
+++ src/controllers/plant.ts
@@ -1,31 +1,49 @@
-import { Request, Response } from "express";
-import { ApiResult } from "interface/api";
+import { Request, Response } from 'express';
+import { ApiResult } from '../interface/api';
+import { PlantData } from '../interface/plant';
+import db from '../models/index';
 
-type plantState = {
-    test:string;
-};
+export const getPlantState = async (
+  req: Request,
+  res: Response,
+): Promise<any> => {
+  let apiResult: ApiResult = {
+    code: 400,
+    data: null,
+    msg: 'Failed',
+  };
 
-export const getPlantState = async(req:Request,res:Response):Promise<any> =>{
-    let apiResult: ApiResult={
-        code:400,
-        data:null,
-        msg:"Failed"
-    };
+  try {
+    const userName = req.body.user_name; //사용자 이름 추출
+    const plantId = req.body.plant_id; //식물 이름 추출
 
-    try{
+    const plantData = await db.sequelize.query(
+      `
+        SELECT p.current_temp, p.current_light, p.current_moisture, p.temp_state, p.light_state, p.moisture_state
+        FROM user u, plant p
+        WHERE u.user_id = p.user_id AND u.user_name = '${userName}' AND p.plant_id = ${plantId};
+      ` ,
+      {
+        type: db.Sequelize.QueryTypes.SELECT,
+      },
+    );
 
-        const plantState:plantState = {
-            test:"test"
-        };
+    console.log(plantData);
 
-        apiResult.code = 200;
-        apiResult.data = plantState;
-        apiResult.msg = "Ok";
-    }catch(err){
-        apiResult.code = 500;
-        apiResult.data = null;
-        apiResult.msg = 'ServerError';
+    if (plantData.length > 0) {
+      apiResult.code = 200;
+      apiResult.data = plantData[0];
+      apiResult.msg = 'Ok';
+    } else {
+      apiResult.code = 404;
+      apiResult.msg = 'Not Exits PlantData';
     }
+  } catch (err) {
+    apiResult.code = 500;
+    apiResult.data = null;
+    apiResult.msg = 'ServerError';
+    console.log(err);
+  }
 
-    res.json(apiResult);
-}
+  res.json(apiResult);
+};
