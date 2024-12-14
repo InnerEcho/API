ecf3b7755375b95691b23678a75ea0808386d584
YonghoBae <dyddyd134@chungbuk.ac.kr>
2024-12-14 20:25:21 +0900

    modify

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import { DbConfig } from "../interface/db";
 
+console.log("sdfsdfsdfs",process.env.DB_PW);
+
 const development: DbConfig = {
   username: 'root',
-  password: `${process.env.DB_PW}`,
-  database: `${process.env.DB_NAME}`,
+  password: '@as060131',
+  database: 'innerecho',
   host: 'localhost',
   dialect: 'mysql',
   port: 3306,
@@ -14,7 +16,7 @@ const development: DbConfig = {
 
 const test: DbConfig = {
   username: 'root',
-  password: `${process.env.DB_PW}`,
+  password: '@as060131',
   database: `${process.env.DB_NAME}`,
   host: 'localhost',
   dialect: 'mysql',
@@ -25,7 +27,7 @@ const test: DbConfig = {
 
 const production: DbConfig = {
   username: 'root',
-  password: `${process.env.DB_PW}`,
+  password: '@as060131',
   database: `${process.env.DB_NAME}`,
   host: 'localhost',
   dialect: 'mysql',
