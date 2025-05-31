ce70d35dc88fea81e223ffe0ac639e53e326d251
HyunWooNo <zz47911@naver.com>
2025-05-31 21:47:23 +0900

    flask

--- src/controllers/ChatBotController.ts
+++ src/controllers/ChatBotController.ts
@@ -34,4 +34,20 @@ export class PlantChatBotController {
       res.status(500).json(result);
     }
   }
+  // state 저장 호출출
+  async emotionSave(req: Request, res: Response) {
+    try {
+      const { user_Id, plant_Id, message, emotion } = req.body;
+
+      // ChatService의 create 메서드 호출
+      await this.chatService.create(user_Id, plant_Id, message, emotion);
+
+      // 필요하다면 추가 로직 및 응답 처리
+      res.status(200).json({ code: 200, msg: 'Success' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ code: 500, msg: 'Server Error' });
+    }
+  }
+
 }
